d3fa2e79f92a05646b542296b491c31d3d935f76
唐海波 <haibo.tang@ymm56.com>
2018-06-15 16:17:06 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ const { spawn } = require('child_process');
 let cmd = spawn('npm', ['run', 'git']);
 
 cmd.stdout.on('data', (result) => {
-  console.log(result)
+  console.log(result.toString())
 })
