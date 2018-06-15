09ff6cf184041569569154fdd73f44caee398f23
唐海波 <haibo.tang@ymm56.com>
2018-06-15 16:21:36 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 const winston = require('winston');
 const { spawn } = require('child_process');
+const fs = require('fs');
 
 let cmd = spawn('npm', ['run', 'git']);
 
-cmd.stdout.on('data', (result) => {
-  console.log(result.toString())
+fs.appendFile(__dirname + '1.txt', new Date(), err => {
+  if(err){
+    // err handle
+  }
 })
+// cmd.stdout.on('data', (result) => {
+//   console.log(result.toString())
+// })
