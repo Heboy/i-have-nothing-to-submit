7149df790c48a6a44e3d55fdd191a78dcc13d123
唐海波 <haibo.tang@ymm56.com>
2018-06-15 16:32:23 +0800

    update

--- src/main.js
+++ src/main.js
@@ -9,12 +9,7 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `combined.log`
-    // - Write all logs error (and below) to `error.log`.
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
+    new winston.transports.File({ filename: 'error.log', level: 'error' })
   ]
 });
 
@@ -27,7 +22,7 @@ fs.appendFile(path.resolve(__dirname, '../1.txt'), new Date(), err => {
     })
   }
   logger.log({
-    level: 'error',
+    level: 'info',
     message: 'err'
   })
 })
